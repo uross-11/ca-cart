c67e7bc308e616a814647946b9ce0c4d77798809
Uros Ignjatovic <u.ignjatovic00@gmail.com>
2021-06-04 16:29:48 +0200

    Add fetch

--- src/context.js
+++ src/context.js
@@ -21,15 +21,26 @@ const AppProvider = ({ children }) => {
 
   const remove = (id) => {
     dispatch({type: 'REMOVE', payload: id});
-  }
+  };
 
   const increase = (id) => {
     dispatch({type: 'INCREASE', payload: id})
-  }
+  };
 
   const decrease = (id) => {
     dispatch({type: 'DECREASE', payload: id})
-  }
+  };
+
+  const fetchData = async () => {
+    dispatch({type: 'LOADING'})
+    const response = await fetch(url);
+    const cart = await response.json();
+    dispatch({type: 'DISPLAY_ITEMS', payload: cart})
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [])
 
   useEffect(() => {
     dispatch({type: 'GET_TOTALS'})
